e2c71ad01615f73364d7e9a564453dff85493a40
djavorszky <daniel.javorszky@liferay.com>
2017-03-17 16:59:00 +0100

    Add JDBC url generation and tests

---
+++ liferay_test.go
@@ -0,0 +1,114 @@
+package liferay
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestOracleJDBC(t *testing.T) {
+	control := JDBC{
+		Driver:   "jdbc.default.driverClassName=oracle.jdbc.driver.OracleDriver",
+		URL:      "jdbc.default.url=jdbc:oracle:thin:@192.168.211.88:1521:orcl",
+		User:     "jdbc.default.username=system",
+		Password: "jdbc.default.password=password",
+	}
+
+	jdbc := OracleJDBC("192.168.211.88", "1521", "orcl", "system", "password")
+
+	if jdbc.Driver != control.Driver {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.Driver, jdbc.Driver))
+	}
+
+	if jdbc.URL != control.URL {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.URL, jdbc.URL))
+	}
+
+	if jdbc.User != control.User {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.User, jdbc.User))
+	}
+
+	if jdbc.Password != control.Password {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.Password, jdbc.Password))
+	}
+}
+
+func TestPostgresJDBC(t *testing.T) {
+	control := JDBC{
+		Driver:   "jdbc.default.driverClassName=org.postgresql.Driver",
+		URL:      "jdbc.default.url=jdbc:postgresql://192.168.211.88:5432/lportal",
+		User:     "jdbc.default.username=system",
+		Password: "jdbc.default.password=password",
+	}
+
+	jdbc := PostgreJDBC("192.168.211.88", "5432", "lportal", "system", "password")
+
+	if jdbc.Driver != control.Driver {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.Driver, jdbc.Driver))
+	}
+
+	if jdbc.URL != control.URL {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.URL, jdbc.URL))
+	}
+
+	if jdbc.User != control.User {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.User, jdbc.User))
+	}
+
+	if jdbc.Password != control.Password {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.Password, jdbc.Password))
+	}
+}
+
+func TestMysqlJDBC(t *testing.T) {
+	control := JDBC{
+		Driver:   "jdbc.default.driverClassName=com.mysql.jdbc.Driver",
+		URL:      "jdbc.default.url=jdbc:mysql://192.168.211.88:3306/lportal?useUnicode=true&characterEncoding=UTF-8&useFastDateParsing=false",
+		User:     "jdbc.default.username=root",
+		Password: "jdbc.default.password=password",
+	}
+
+	jdbc := MysqlJDBC("192.168.211.88", "3306", "lportal", "root", "password")
+
+	if jdbc.Driver != control.Driver {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.Driver, jdbc.Driver))
+	}
+
+	if jdbc.URL != control.URL {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.URL, jdbc.URL))
+	}
+
+	if jdbc.User != control.User {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.User, jdbc.User))
+	}
+
+	if jdbc.Password != control.Password {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.Password, jdbc.Password))
+	}
+}
+
+func TestMysqlJDBCDXP(t *testing.T) {
+	control := JDBC{
+		Driver:   "jdbc.default.driverClassName=com.mysql.jdbc.Driver",
+		URL:      "jdbc.default.url=jdbc:mysql://192.168.211.88:3306/lportal?characterEncoding=UTF-8&dontTrackOpenResources=true&holdResultsOpenOverStatementClose=true&useFastDateParsing=false&useUnicode=true",
+		User:     "jdbc.default.username=root",
+		Password: "jdbc.default.password=password",
+	}
+
+	jdbc := MysqlJDBCDXP("192.168.211.88", "3306", "lportal", "root", "password")
+
+	if jdbc.Driver != control.Driver {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.Driver, jdbc.Driver))
+	}
+
+	if jdbc.URL != control.URL {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.URL, jdbc.URL))
+	}
+
+	if jdbc.User != control.User {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.User, jdbc.User))
+	}
+
+	if jdbc.Password != control.Password {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.Password, jdbc.Password))
+	}
+}
