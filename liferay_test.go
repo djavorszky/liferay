12b06fc1564757f038f1ba79d35b6329362eabe3
Daniel Javorszky <daniel.javorszky@liferay.com>
2018-05-30 16:06:38 +0200

    Add DXP specific mssql jdbc strings

--- liferay_test.go
+++ liferay_test.go
@@ -139,3 +139,30 @@ func TestMSSQLJDBC(t *testing.T) {
 		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.Password, jdbc.Password))
 	}
 }
+
+func TestMSSQLJDBCDXP(t *testing.T) {
+	control := JDBC{
+		Driver:   "jdbc.default.driverClassName=com.microsoft.sqlserver.jdbc.SQLServerDriver",
+		URL:      "jdbc.default.url=jdbc:sqlserver://192.168.211.88:1433;databaseName=master",
+		User:     "jdbc.default.username=clouddb",
+		Password: "jdbc.default.password=password",
+	}
+
+	jdbc := MSSQLJDBCDXP("192.168.211.88:1433", "master", "clouddb", "password")
+
+	if jdbc.Driver != control.Driver {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.Driver, jdbc.Driver))
+	}
+
+	if jdbc.URL != control.URL {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.URL, jdbc.URL))
+	}
+
+	if jdbc.User != control.User {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.User, jdbc.User))
+	}
+
+	if jdbc.Password != control.Password {
+		t.Errorf(fmt.Sprintf("Mismatch: Expected %q, got %q", control.Password, jdbc.Password))
+	}
+}
